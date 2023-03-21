8acecbfa356734bb3b82600c3789407ddd358765
RunfengLin815 <1661442416@qq.com>
2023-03-21 15:38:23 +0800

    Save aligned depth and color to disk as png.

--- demo.cpp
+++ demo.cpp
@@ -3,79 +3,56 @@
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
 
+#include <opencv2/opencv.hpp>
+
 #include <iostream>
 #include <string>
-#include <dirent.h>
 
 // 3rd party header for writing png files
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
 
-// This example assumes camera with depth and color streams, and direction lets you define the target stream
-enum class direction
-{
-    to_depth,
-    to_color
-};
-
-
-void vf2png (rs2::video_frame vf);
 
 int main () {
-    // Create a simple OpenGL window
-    window app(1280, 720, "RealSense Record");
-    ImGui_ImplGlfw_Init(app, false);
 
     // create two textures on gpu
     texture img_col, img_dep;
     // create color map since depth data usually grey
     rs2::colorizer color_map;
 
-    // Create a shared pointer to a pipeline ("shared is for recording and saving")
-    auto pipe = std::make_shared<rs2::pipeline>();
-
-    // create configuration
-    rs2::config cfg;
-    // init pipeline, get stream from depth and color for alignment
-    cfg.enable_stream(RS2_STREAM_DEPTH);
-    cfg.enable_stream(RS2_STREAM_COLOR);
+    // create a pipeline
+    rs2::pipeline pipe = rs2::pipeline();
 
     // start shared pipeline with config
-    pipe->start(cfg);
-
-    // Initialize a shared pointer to a device with the current device on the pipeline
-    rs2::device device = pipe->get_active_profile().get_device();
+    pipe.start();
 
     // init alignment
     // define align object (out of main loop)
     rs2::align align_to_depth(RS2_STREAM_DEPTH);
     rs2::align align_to_color(RS2_STREAM_COLOR);
 
-    float       alpha = 0.5f;               // Transparancy coefficient
-
-    // main loop, if app existing, keep running
+    // main loop
     int i=0;
     while ( i < 1 ) {
         // wait for next set of frames from the camera
-        rs2::frameset frameset = pipe->wait_for_frames();
+        rs2::frameset frameset = pipe.wait_for_frames();
 
         // Align all frames to depth viewport
         frameset = align_to_color.process(frameset);
 
         // get color data
-        auto color = frameset.get_color_frame();
-
+        auto color_frame = frameset.get_color_frame();
         // get depth data
-        auto depth = frameset.get_depth_frame();
-        auto colorized_depth = color_map.colorize(depth);
+        auto depth_frame = frameset.get_depth_frame();
 
-        // rendering and show in window
-        img_dep.render(colorized_depth, { 0, 0, app.width(), app.height() });
-        img_col.render(color, { 0, 0, app.width(), app.height() }, alpha);
+        // write depth to disk
+        cv::Mat depth_img(depth_frame.get_height(), depth_frame.get_width(), CV_16UC1, (void *)depth_frame.get_data());
+        cv::imwrite("depth_img.png", depth_img);
 
-        // Write images to disk
-        vf2png(color);
-        vf2png(colorized_depth);
+        // write color to disk
+        cv::Mat color_img(color_frame.get_height(), color_frame.get_width(), CV_8UC3, (void *)color_frame.get_data());
+        cv::cvtColor(color_img, color_img, cv::COLOR_RGB2BGR);
+        cv::imwrite("color_img.png", color_img);
 
         i++;
 
@@ -84,15 +61,6 @@ int main () {
 
 
     // return
-    return EXIT_SUCCESS;
+    return 0;
 
-}
-
-void vf2png (const rs2::video_frame vf) {
-    std::stringstream png_file;
-    png_file << "rs-demo-" << vf.get_profile().stream_name() << ".png";
-    stbi_write_png(png_file.str().c_str(), vf.get_width(), vf.get_height(),
-                   vf.get_bytes_per_pixel(), vf.get_data(), vf.get_stride_in_bytes());
-//    std::string prefix = "/home/linrunfeng/Data/rs_save_demo";
-    std::cout << "Saved " << png_file.str() << std::endl;
 }
